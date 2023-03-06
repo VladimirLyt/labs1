450c0ad3f1ab2970b6a19dd08f0fef7d717227ed
lab3 <dom56f@gmail.com>
2023-03-07 06:17:53 +0900

    test

---
+++ pythonProject/main.py
@@ -0,0 +1,35 @@
+#Этолаба3
+def prog1():
+    a=[]
+    while (b:=str(input()))!="stop":
+        a.append(b)
+    print(" ".join(a))
+prog1()
+def prog2():
+    while (b:=str(input()))!="stop":
+        if "ф" in b or "Ф" in b:
+            print("Ого это редкое слово")
+        else:
+            print("Эх, это не очень редкое слово")
+prog2()
+def prog3():
+    from random import randint
+    mi=0
+    rh=0
+    while mi!=3:
+        a = randint(1, 10)
+        b = randint(1, 10)
+        summ=a+b
+        print(a,"+",b)
+        f=int(input())
+        if f == summ:
+            print("Правильно")
+            rh+=1
+        if f==0:
+            print("Игра приостановлена")
+            break
+        else:
+            mi+=1
+            print("Ответ неправильный")
+    print("Игра окончена", "Правильных ответов", rh)
+prog3()
